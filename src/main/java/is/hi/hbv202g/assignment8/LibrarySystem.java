0b343c5563b866ff0e15517c9142fcf47b1b33ec
sunnakristin <sunnakristing@gmail.com>
2024-03-08 09:03:11 +0000

    user

--- src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
+++ src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
@@ -1,3 +1,5 @@
 package is.hi.hbv202g.assignment8;
+
 public class LibrarySystem {
+
 }
---
+++ src/main/java/is/hi/hbv202g/assignment8/User.java
@@ -0,0 +1,19 @@
+package is.hi.hbv202g.assignment8;
+
+public abstract class User {
+
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
