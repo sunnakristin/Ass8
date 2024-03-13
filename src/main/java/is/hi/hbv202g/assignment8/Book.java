b883f4e2d63bdbbc69ab7d3cdbaf26b5c27951cd
Sabbalingurinn <s.b.snorrason@gmail.com>
2024-03-13 12:56:58 +0000

    komið

--- src/main/java/is/hi/hbv202g/assignment8/Book.java
+++ src/main/java/is/hi/hbv202g/assignment8/Book.java
@@ -7,15 +7,15 @@ public class Book {
     private String title;
     private List<Author> authors;
 
-    public Book(String title, String authorName) throws EmptyAuthorListException {
-        if (authorName == null || authorName.isEmpty()) {
-            throw new EmptyAuthorListException("Author name cannot be null or empty.");
-        }
+    public Book(String title, String authorName) {
         this.title = title;
         this.authors = List.of(new Author(authorName));
     }
 
-    public Book(String title, List<Author> authors) {
+    public Book(String title, List<Author> authors) throws EmptyAuthorListException {
+        if (authors == null || authors.isEmpty()) {
+            throw new EmptyAuthorListException("Author list cannot be null or empty.");
+        }
         this.title = title;
         this.authors = authors;
     }
