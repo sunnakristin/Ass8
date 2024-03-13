4356aea52217153a0c45342b78344f0fb7fd2e5c
Sabbalingurinn <s.b.snorrason@gmail.com>
2024-03-13 12:51:18 +0000

    loka push

--- src/main/java/is/hi/hbv202g/assignment8/Main.java
+++ src/main/java/is/hi/hbv202g/assignment8/Main.java
@@ -1,5 +1,6 @@
 package is.hi.hbv202g.assignment8;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -8,11 +9,6 @@ import java.util.List;
  */
 public class Main {
     public static void main(String[] args) throws EmptyAuthorListException {
-        LibrarySystem myLibrarySystem = new LibrarySystem();
 
-        myLibrarySystem.addBookWithTitleAndAuthorList("The Hobbit", List.of(new Author("JRR Tolkien")));
-        myLibrarySystem.addBookWithTitleAndAuthorList("The Fellowship of the Ring", List.of(new Author("JRR Tolkien")));
-        myLibrarySystem.addBookWithTitleAndAuthorList("The Two Towers", List.of(new Author("JRR Tolkien")));
-        myLibrarySystem.addBookWithTitleAndAuthorList("The Return of the King", List.of(new Author("JRR Tolkien")));
     }
 }
