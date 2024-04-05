cdcc24f7a0748bf60f90c5dc86bd6f5c8d83e687
sunnakristin <sunnakristing@gmail.com>
2024-04-05 11:24:35 +0000

    testum bætt við í lending

--- src/test/java/is/hi/hbv202g/assignment8/LendingTest.java
+++ src/test/java/is/hi/hbv202g/assignment8/LendingTest.java
@@ -1,5 +1,61 @@
 package is.hi.hbv202g.assignment8;
 
+import java.time.LocalDate;
+
+import org.junit.Test;
+
 public class LendingTest {
-    
+
+    @Test
+    public void getDueDateTest() {
+        Book book = new Book("Title", "Author");
+        User user = new Student("Name", true);
+        Lending lending = new Lending(book, user);
+        assert lending.getDueDate().equals(LocalDate.now().plusDays(30));
+    }
+
+    @Test
+    public void setDueDateTest() {
+        Book book = new Book("Title", "Author");
+        User user = new Student("Name", true);
+        Lending lending = new Lending(book, user);
+        lending.setDueDate(LocalDate.now().plusDays(2));
+        assert lending.getDueDate().equals(LocalDate.now().plusDays(2));
+    }
+
+    @Test
+    public void getBookTest() {
+        Book book = new Book("Title", "Author");
+        User user = new Student("Name", true);
+        Lending lending = new Lending(book, user);
+        assert lending.getBook().equals(book);
+    }
+
+    @Test
+    public void setBookTest() {
+        Book book = new Book("Title", "Author");
+        User user = new Student("Name", true);
+        Lending lending = new Lending(book, user);
+        Book newBook = new Book("New Title", "New Author");
+        lending.setBook(newBook);
+        assert lending.getBook().equals(newBook);
+    }
+
+    @Test
+    public void getUserTest() {
+        Book book = new Book("Title", "Author");
+        User user = new Student("Name", true);
+        Lending lending = new Lending(book, user);
+        assert lending.getUser().equals(user);
+    }
+
+    @Test
+    public void setUserTest() {
+        Book book = new Book("Title", "Author");
+        User user = new Student("Name", true);
+        Lending lending = new Lending(book, user);
+        User newUser = new Student("New Name", true);
+        lending.setUser(newUser);
+        assert lending.getUser().equals(newUser);
+    }
 }
